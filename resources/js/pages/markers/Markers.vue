a2f9e2465423f4b48d7421daebab56e9ec9f4581
Dino <dino8890@protonmail.com>
2022-05-03 14:29:29 +0200

    Stop video when closing video preview modal using X button.

--- resources/js/pages/markers/Markers.vue
+++ resources/js/pages/markers/Markers.vue
@@ -225,7 +225,7 @@
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" v-show="!editmode" >{{$t('preview')}}</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <button type="button" class="close" data-dismiss="modal" @click="stopVideo" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
